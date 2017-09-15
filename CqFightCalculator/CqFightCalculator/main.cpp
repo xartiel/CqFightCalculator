3179d77a22bb0c3c6231e6b6bca26b5c1aa26efe
xartiel <arteel@free.fr>
2017-09-15 04:36:20 +0200

    Nope, unclear

---
+++ CqFightCalculator/CqFightCalculator/main.cpp
@@ -0,0 +1,17 @@
+// CosmosQuest fights calculator.
+// By Xartiel.
+//
+// Tools.DiffFiles
+
+#include <vector>
+#include <string>
+
+
+
+//	Plan:
+//	  . Read config
+//	  . 
+int		main()
+{
+  return (EXIT_SUCCESS);
+}
